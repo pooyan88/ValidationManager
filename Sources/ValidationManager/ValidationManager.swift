e9089d3860c1886e350a55b02a1d159b3d524a50
Pooyan J <pooyanjavidi@gmail.com>
2025-08-06 15:30:57 +0330

    removed duplicated file

--- Sources/ValidationManager/ValidationManager.swift
+++ Sources/ValidationManager/ValidationManager.swift
@@ -1,2 +1,18 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Foundation
+
+public class ValidationManager: Validator {
+
+    var strategy: Validator
+
+    init(strategy: Validator) {
+        self.strategy = strategy
+    }
+
+    public func validate() -> (isValid: Bool, error: String?) {
+        return strategy.validate()
+    }
+}
+

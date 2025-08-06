211a2ed7af6d2c3e65d99cb6a64cf69f51bdd421
Pooyan J <pooyanjavidi@gmail.com>
2025-08-06 13:03:09 +0330

    phone validator

--- Sources/ValidationManager/Protocol/Validator.swift
+++ Sources/ValidationManager/Protocol/Validator.swift
@@ -6,3 +6,7 @@
 //
 
 import Foundation
+
+public protocol Validator {
+    func validate() -> (isValid: Bool, error: String?)
+}
--- Sources/ValidationManager/Strategies/PhoneNumberValidator.swift
+++ Sources/ValidationManager/Strategies/PhoneNumberValidator.swift
@@ -6,3 +6,41 @@
 //
 
 import Foundation
+
+public class PhoneNumberValidation: Validator {
+
+    public struct Config {
+        var phoneNumber: String
+        var invalidNumberError: String
+        var invalidNumberLengthError: String
+    }
+
+    public var config: Config
+
+    public var isPhoneNumberValid: Bool {
+        return validate().isValid
+    }
+
+    public init(config: Config) {
+        self.config = config
+    }
+
+    public func validate() -> (isValid: Bool, error: String?) {
+            let phone = config.phoneNumber.trimmingCharacters(in: .whitespacesAndNewlines)
+            guard !phone.isEmpty else { return (true, nil) }
+
+            let startsWithZero = phone.hasPrefix("0")
+            let startsWithNine = phone.hasPrefix("9")
+
+            guard startsWithZero || startsWithNine else {
+                return (false, config.invalidNumberError)
+            }
+            if startsWithZero && phone.count != 11 {
+                return (false, config.invalidNumberLengthError)
+            }
+            if startsWithNine && phone.count != 10 {
+                return (false, config.invalidNumberLengthError)
+            }
+            return (true, nil)
+        }
+}
